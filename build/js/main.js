8450fc07c3b30f3c303a6123f61d0ac840b60cfe
Pawan C <pawan.c@marginframe.com>
2014-02-17 14:24:26 +0700

    re-add all files

---
+++ build/js/main.js
@@ -0,0 +1,21 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+$("#main-banner").owlCarousel({
+
+  navigation : true, // Show next and prev buttons
+  slideSpeed : 300,
+  paginationSpeed : 400,
+  singleItem:true
+
+  // "singleItem:true" is a shortcut for:
+  // items : 1, 
+  // itemsDesktop : false,
+  // itemsDesktopSmall : false,
+  // itemsTablet: false,
+  // itemsMobile : false
+
+});
